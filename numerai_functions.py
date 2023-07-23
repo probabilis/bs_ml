3dbd50fc0297ab1cc9df8fb8ef2b65be03592afb
probabilis <m.gschaider@outlook.com>
2023-07-23 14:30:20 +0200

    updated

--- numerai_functions.py
+++ numerai_functions.py
@@ -1,8 +1,2 @@
 import numpy as np
 
-def numerai_score(y, y_pred):
-	rank_pred = y_pred.groupby(eras).apply(lambda x: x.rank(pct = True, method = "first") )
-	return np.corrcoef(y, rank_pred)[0,1]
-
-def correlation_score(y, y_pred):
-	return np.corrcoef(y, y_pred)[0,1]

fbe4bce181d0536dd72bbe76e99b561622359b85
probabilis <m.gschaider@outlook.com>
2023-07-23 15:39:52 +0200

    updated

--- analysis/cv_analysis.py
+++ analysis/cv_analysis.py
@@ -52,7 +52,7 @@ def cross_validation(X,Y,eras, crossvalidators):
 
     for cv in crossvalidators:
         print(cv)
-        score = cross_val_score(LGBMRegressor(params_gbm),X,Y, cv = cv, n_jobs = 1, groups = eras, scoring = metrics.make_scorer(correlation_score, greater_is_better = True))
+        score = cross_val_score(LGBMRegressor(**params_gbm),X,Y, cv = cv, n_jobs = 1, groups = eras, scoring = metrics.make_scorer(correlation_score, greater_is_better = True))
         print(np.mean(score))
         print('-------')
     return

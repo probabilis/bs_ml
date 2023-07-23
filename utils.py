ae839380226e906257da0ce1f79a877c3d306105
probabilis <m.gschaider@outlook.com>
2023-07-23 20:55:44 +0200

    updated

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import pandas as pd
 import numpy as np
 
@@ -14,3 +15,34 @@ def numerai_score(y, y_pred):
 
 def correlation_score(y, y_pred):
 	return np.corrcoef(y, y_pred)[0,1]
+
+
+
+
+MODEL_FOLDER = "models"
+
+
+def save_model(model, mtype, params): 
+    if mtype == "LGBM":
+        try:
+            Path(MODEL_FOLDER).mkdir(exist_ok = True, parents = True)
+        except Exception as ex:
+            pass
+        model.booster_.save_model(f"{MODEL_FOLDER}/{mtype}_{params}.json")
+    if mtype == "XGB":
+        try:
+            Path(MODEL_FOLDER).mkdir(exist_ok = True, parents = True)
+        except Exception as ex:
+            pass
+        model.booster_.save_model(f"{MODEL_FOLDER}/{mtype}_{params}.json")
+    if mtype == "CatBoost":
+        try:
+            Path(MODEL_FOLDER).mkdir(exist_ok = True, parents = True)
+        except Exception as ex:
+            pass
+        model.save_model(f"{MODEL_FOLDER}/{mtype}_{params}.json", format = "json")
+
+
+
+
+

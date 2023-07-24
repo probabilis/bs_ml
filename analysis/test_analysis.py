512e4707fbb5dffa7581d3f43ff4ac2cbc7d0273
probabilis <m.gschaider@outlook.com>
2023-07-24 17:31:17 +0200

    updated

---
+++ analysis/test_analysis.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np
+
+from utils import path
+
+
+df = pd.read_parquet(path)
+features = [f for f in df if f.startswith("feature")]
+target = "target"
+df["erano"] = df.era.astype(int)
+eras = df.erano
+
+
+def feature_corr(df, era_col, target_col):
+    all_feature_corrs = df.groupby(era_col).apply(
+    lambda era: era[features].corrwith(era[target_col]))
+    return all_feature_corrs
+
+print( feature_corr(df, "era", "target"))

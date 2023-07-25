3e305d6a06dd56ccd1054428fc1de8ce41a65436
probabilis <m.gschaider@outlook.com>
2023-07-25 16:37:58 +0200

    updated

--- subprocess_program.py
+++ subprocess_program.py
@@ -5,12 +5,14 @@ version = "python3"
 
 #input_path = "bayesian_opt/"
 #input_file = 'bayesian_optimization.py'
+#input_path = "analysis/"
+#input_file = 'cv_analysis.py'
 
-input_path = "analysis/"
-input_file = 'cv_analysis.py'
+input_path = input("Enter your input path of input file starting at bs_ml/...")
+input_file = input("Enter your input file.")
 
 output_path = "outputs/"
-output_file = f'cv_analysis_output_{date.today()}.txt'
+output_file = f'{input_file}_output_{date.today()}.txt'
 
 ###########################################
 

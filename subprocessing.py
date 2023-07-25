c4616ebf2273c204059b67365860218d36cb496a
probabilis <m.gschaider@outlook.com>
2023-07-25 19:06:49 +0200

    updated

--- subprocessing.py
+++ subprocessing.py
@@ -1,8 +1,7 @@
-
 import subprocess
 
-path = "bayesian_opt/"
+path_ = "bayesian_opt/"
 program = "bayesian_optimization.py"
 
 with open("output.txt", "wb") as f:
-    subprocess.check_call(["python3", path + program], stdout=f)
+    subprocess.check_call(["python3", path_ + program], stdout=f)

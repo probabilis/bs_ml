4d76bff0173ac031cbf44f6cb4280311518e4125
probabilis <m.gschaider@outlook.com>
2023-07-25 16:14:17 +0200

    updated

--- analysis/statistical_analysis.py
+++ analysis/statistical_analysis.py
@@ -28,7 +28,7 @@ print('mean of ' + str(x), mean)
 
 plot_statistics(df_st,'mean', "train_df_features_mean", path_ = "/figures")
 plot_statistics(df_st,'variance', "train_df_features_variance", path_ = "/figures")
-
+"""
 histogram(df_st['feature_mean'], "train_df_hist_mean", path_ = "/figures")
 histogram(df_st['feature_variance'], "train_df_hist_var", path_ = "/figures")
 
@@ -36,7 +36,7 @@ histogram(x, features[0])
 x.plot(kind = 'box')
 plt.show()
 
-"""
+
 skews = get_skewed_columns(df[features])
 first = skews.index[0]
 print(first)
--- analysis/test_analysis.py
+++ analysis/test_analysis.py
@@ -1,26 +1,13 @@
 import sys
 import pandas as pd
 sys.path.append('../')
-from utils import loading_dataset, get_biggest_change_features
+from utils import loading_dataset, feature_corr, get_biggest_change_features
 
 #############################################
 
 df, features, target, eras = loading_dataset()
 
 #############################################
-
-def feature_corr(df, era_col, target_col):
-    """
-    params: df, era_col, target_col 
-    era_col ...     era column
-    target_col ...  target column
-    ---------------
-    return: all_feature_corr
-    df ...          dataframe with correlation from all features  
-    """
-    all_feature_corrs = df.groupby(era_col).apply(
-    lambda era: era[features].corrwith(era[target_col]))
-    return all_feature_corrs
 
 corrs_ = feature_corr(df, "era", "target")
 

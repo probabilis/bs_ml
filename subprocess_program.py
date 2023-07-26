1cd61179be4d2e8350d6f3e46afb38151c67e259
probabilis <m.gschaider@outlook.com>
2023-07-26 12:35:13 +0200

    updated

--- subprocess_program.py
+++ subprocess_program.py
@@ -22,11 +22,19 @@ total_ipath = input_path + input_file + ".py"
 total_opath = output_path + output_file
 
 # Execute the Python script and capture its output
-result = subprocess.run([version, total_ipath], capture_output=True, text=True)
+result = subprocess.run([version, total_ipath], capture_output=True, text=True, stderr = subprocess.STDOUT, stdout = subprocess.PIPE)
 output_text = result.stdout
 
 # Write the output to a text file
 with open(total_opath, 'w') as file:
-    file.write(output_text)
+    try:
+        file.write(output_text)
+        if result.returncode != 0: 
+            file.write(f"Script execution error (return code: {result.returncode})")
+
+    except FileNotFoundError:
+        file.write("Error: Python executable 'python' not found. Make sure Python is installed and added to PATH.")
+
+
 
 print(f"Output written to {total_opath}.")

80b6133cb2970d963214a9331546d0cd6722f6e9
probabilis <m.gschaider@outlook.com>
2023-09-04 20:41:34 +0200

    updated

--- from_scratch/decision_trees_comparison.py
+++ from_scratch/decision_trees_comparison.py
@@ -4,54 +4,72 @@ MN: 12030366
 """
 #official open-source repositories
 from sklearn.tree import DecisionTreeRegressor, export_graphviz
+from sklearn import tree
 import pandas as pd
+import sys
 import matplotlib.pyplot as plt
 #own modules
 from decision_regression_tree_from_scratch import DecisionTreeRegressorScratch
 from testfunction import testfunction, X
+sys.path.append('../')
+from bs_ml.utils import repo_path, fontsize_title, fontsize
 
 ###################################
 
-max_depth = 1
+max_depth = 2
 
 Y = testfunction(X, noise = 0.5)
 X = X.reshape(-1,1)
 
-model = DecisionTreeRegressor(max_depth = max_depth)
-model.fit(X,Y)
-y_pred = model.predict(X)
+###################################
 
-model_scratch = DecisionTreeRegressorScratch(pd.DataFrame(X), Y.tolist(), max_depth = max_depth)
-model_scratch.grow_tree()
-y_pred_scratch = model_scratch.fit(pd.DataFrame(X))
+def dt_scratch_sklearn_comparison(plot_save) -> None:
 
-################################################
+    fig, axs = plt.subplots(1)
+    fig.set_size_inches(12,8)
 
-fig, ax = plt.subplots(1)
-fig.set_size_inches(12,8)
+    model = DecisionTreeRegressor(max_depth = max_depth)
+    model.fit(X,Y)
+    y_pred = model.predict(X)
 
-fig.suptitle("Decision Tree Regressor from Scratch comparison with SKLEARN module")
-ax.set_title("max depth $d_{max}$ = " + str(max_depth))
-ax.scatter(X, Y, color = 'gray')
-ax.plot(X, y_pred, color = 'salmon', linestyle = ':', linewidth = 3, label = 'SKLEARN')
-ax.plot(X, y_pred_scratch, color = 'mediumseagreen', linestyle = '--',linewidth = 3, label = 'SCRATCH')
-ax.legend()
-fig.tight_layout()
-#plt.savefig("decision_tree_regressor_comparison.png", dpi=300)
-plt.show()
+    model_scratch = DecisionTreeRegressorScratch(pd.DataFrame(X), Y.tolist(), max_depth = max_depth)
+    model_scratch.grow_tree()
+    y_pred_scratch = model_scratch.fit(pd.DataFrame(X))
 
-from sklearn import tree
+    ###################################
 
 
-#import graphviz
-#dot_data = export_graphviz(model, out_file = None)
-#graph = graphviz.Source(dot_data, filename = 'test.gv', format = 'png')
-#graph.view()
+
+    fig.suptitle("Decision Tree Regressor from Scratch comparison with SKLEARN module / max. depth $d_{max}$ = " + str(max_depth), fontsize = fontsize_title)
+    
+    axs.scatter(X, Y, color = 'gray')
+    axs.plot(X, y_pred, color = 'salmon', linestyle = ':', linewidth = 3, label = 'SKLEARN')
+    axs.plot(X, y_pred_scratch, color = 'mediumseagreen', linestyle = '--',linewidth = 3, label = 'SCRATCH')
+    axs.set_xlabel("$x$")
+    axs.set_ylabel("$F(x)$")
+    axs.legend(fontsize = fontsize)
+    
+    fig.tight_layout()
+    if plot_save == True:
+        plt.savefig(repo_path + "/figures/" + f"decision_tree_regressor_comparison_md={max_depth}.png", dpi=300)
+
+    plt.show()
+
+    #import graphviz
+    #dot_data = export_graphviz(model, out_file = None)
+    #graph = graphviz.Source(dot_data, filename = 'test.gv', format = 'png')
+    #graph.view()
+
+    fig, axs = plt.subplots(1)
 
 
-tree.plot_tree(model)
-fig = plt.gcf()
-#fig.set_size_inches(16,14)
-fig.tight_layout()
-#plt.savefig("decision_tree_plot.png")
-plt.show()
+    tree.plot_tree(model, fontsize = fontsize)
+    fig = plt.gcf()
+    fig.suptitle("Decision Tree splitting structure / max. depth $d_{max}$ = " + str(max_depth), fontsize = fontsize_title)
+    fig.set_size_inches(12,8)
+    fig.tight_layout()
+    if plot_save == True:
+        plt.savefig(repo_path + "/figures/" + f"decision_tree_splitting_structure_md={max_depth}.png", dpi=300)
+    plt.show()
+
+dt_scratch_sklearn_comparison(plot_save = True)
--- from_scratch/gbm-sklearn_comparison.py
+++ from_scratch/gbm-sklearn_comparison.py
@@ -8,13 +8,16 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 #own modules 
 from gradient_boosting_from_scratch import GradientBoosting
 from testfunction import testfunction, X
+sys.path.append('../')
+from bs_ml.utils import repo_path, fontsize_title, fontsize
 
 ##################################
 
-Y = testfunction(X, noise = 1)
+Y = testfunction(X, noise = 0.5)
 
 X = X.reshape(-1,1)
 Y = np.ravel(Y)
@@ -33,8 +36,12 @@ def gbm_scratch_sklearn_comparison(plot_save) -> None:
 
     for model in models:
         model.fit(X,Y)
-        mse = mean_squared_error(Y, model.predict(X))
-        print('MSE of model ' + str(model) + str(' : '),round(mse,5))
+        if model == self_gbm:
+            y_hat, _ = model.predict(X)
+        else:
+            y_hat = model.predict(X)
+        mse = mean_squared_error(Y, y_hat)
+        print('MSE of model ' + str(model) + str(' : '), round(mse,3))
 
     ################################
 
@@ -43,17 +50,18 @@ def gbm_scratch_sklearn_comparison(plot_save) -> None:
     for i, Y_pred in enumerate(sklearn_gbm.staged_predict(X)):
         val_score[i] = sklearn_gbm.loss_(Y, Y.reshape(-1, 1))
 
-    plt.figure(figsize=(12, 6))
-    plt.title('Deviance / MSE over boosting iterations $n$ from SKLEARN / Scratch module')
-    plt.plot(np.arange(n) + 1, sklearn_gbm.train_score_, color = 'salmon', linestyle = '--',label='Training Set Deviance')
-    plt.hlines(mse,0,n,color = 'mediumseagreen', linestyle = '--', label = 'Scratch Deviance at n = ' + str(n))
-    plt.text(80,1,'MSE = ' + str(round(mse,5)))
-    plt.legend(loc='upper right')
-    plt.xlabel('Boosting iterations $n$')
-    plt.ylabel('Deviance / MSE ')
+    plt.figure(figsize=(12, 8))
+    plt.title('Deviance / MSE over boosting iterations $m$ from SKLEARN / Scratch module', fontsize = fontsize_title)
+    plt.plot(np.arange(n) + 1, sklearn_gbm.train_score_, color = 'salmon', linestyle = '--',label='SKLEARN MSE Deviance')
+    plt.hlines(mse, 0, n, color = 'mediumseagreen', linestyle = '--', label = 'Scratch MSE Deviance at n = ' + str(n))
+    plt.text(86, 1.1, 'MSE = ' + str(round(mse,3)), fontsize = 12, bbox = dict(facecolor = 'white', alpha = 0.5))
+    plt.legend(loc='upper right', fontsize = fontsize)
+    plt.xlabel('Boosting iterations $m$ / number of trees $n_{trees}$', fontsize = fontsize)
+    plt.ylabel('Deviance / MSE ', fontsize = fontsize)
     plt.tight_layout()
     if plot_save == True:
-        plt.savefig("gbm_scratch_sklearn_loss_comparison.png", dpi=300)
+        plt.savefig(repo_path + "/figures/" + "gbm_scratch_sklearn_loss_comparison.png", dpi=300)
+
     plt.show()
 
-gbm_scratch_sklearn_comparison(plot_save = False)
+gbm_scratch_sklearn_comparison(plot_save = True)

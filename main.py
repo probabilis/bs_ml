4bae8197da7fb14c98e2e6ded8bd06706497b061
probabilis <m.gschaider@outlook.com>
2023-09-25 16:07:29 +0200

    updated

--- main.py
+++ main.py
@@ -208,7 +208,8 @@ print(summary_metrics_targets_df)
 # Ensemble predictions together with a simple average
 numerai_selected_targets = ["target_cyrus_v4_20", "target_victor_v4_20"]
 
-favorite_targets = numerai_selected_targets.append(target_candidates[0:2])
+#only selecting the last two least correlated variables on which the final model gets trained
+favorite_targets = [element for element in target_candidates[0:2]]
 
 ensemble_cols = [f"prediction_{target}" for target in favorite_targets]
 #ensure that the ensemble score are ranked by percentile (pct = True)

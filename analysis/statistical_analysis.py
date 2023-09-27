48a9c9c91e55a757ebb627f42820647889416d21
probabilis <m.gschaider@outlook.com>
2023-09-27 14:02:44 +0200

    updated

--- analysis/statistical_analysis.py
+++ analysis/statistical_analysis.py
@@ -7,26 +7,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import json
 #own modules
 sys.path.append('../')
-from data_loading import data_loading
-from statistical_analysis_tools import statistics, plot_statistics, histogram, overall_statistics
+from repo_utils import gh_repos_path
+from data_loading import loading_datasets
+from statistical_analysis_tools import statistics, plot_statistics, histogram, overall_statistics, plot_correlations
 from preprocessing.cross_validators import era_splitting
 
 #############################################
 
-train, feature_cols, target_cols = data_loading()
+train, feature_cols, target_cols = loading_datasets()
 
 df = era_splitting(train)
 
-features = feature_cols
+feature_metadata = json.load(open(gh_repos_path + "/features.json")) 
+
+feature_sets = feature_metadata["feature_sets"]
+
+assert train["target"].equals(train["target_cyrus_v4_20"])
+target_names = target_cols[1:]
+targets_df = train[["era"] + target_names]
+
+t20s = [t for t in target_names if t.endswith("_20")]
 
 #############################################
+#############################################
+#############################################
+#overall statistics
 
-df_st = statistics(df, features)
+df_st = statistics(df, feature_cols)
 print(df_st)
 
-mean, var = overall_statistics(df, features)
+mean, var = overall_statistics(df, feature_cols)
 print(mean, var)
 
 #############################################
@@ -38,3 +51,18 @@ histogram(df_st['feature_mean'], "train_df_hist_mean", path_ = "/figures")
 histogram(df_st['feature_variance'], "train_df_hist_var", path_ = "/figures")
 
 
+#############################################
+#############################################
+#############################################
+#TARGET correlations
+
+target_correlations = targets_df[t20s].corr()
+plot_correlations(target_correlations, plot_save = True, name = "target")
+print("target correlation plot successfully created")
+#############################################
+#FEATURE correlations
+
+feature_correlations = train[feature_cols].corr()
+plot_correlations(feature_correlations, plot_save = True, name = "feature")
+print("feature correlation plot successfully created")
+#############################################

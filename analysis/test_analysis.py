289ac1a3160744905422bab9b38b5612025fa493
probabilis <m.gschaider@outlook.com>
2023-11-08 20:13:30 +0100

    updated

--- analysis/test_analysis.py
+++ analysis/test_analysis.py
@@ -1,10 +1,27 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 sys.path.append('../')
-from repo_utils import loading_dataset, feature_corr, get_biggest_change_features
+from repo_utils import loading_dataset, feature_corr, get_biggest_change_features, repo_path
 
 #############################################
 
+df = pd.read_csv(repo_path + "/rounds/" + "val_pred.csv")
+print(len(df))
+x = np.arange(0,len(df))
+
+df = df.drop(columns = "era")
+df.plot()
+
+y = lambda x : x
+#y_ = y(x)
+
+plt.plot(x,y(x))
+plt.show()
+
+#############################################
+"""
 df, features, target, eras = loading_dataset()
 
 #############################################
@@ -14,7 +31,7 @@ corrs_ = feature_corr(df, "era", "target")
 n = 50
 risky = get_biggest_change_features(corrs_, n)
 print(risky)
-
+"""
 
 """
 histogram(x, features[0])

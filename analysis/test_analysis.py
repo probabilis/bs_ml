b16445cbe0738cebfb2cafc75ad5265de92fbcb4
probabilis <m.gschaider@outlook.com>
2023-11-09 11:09:30 +0100

    updated

--- analysis/test_analysis.py
+++ analysis/test_analysis.py
@@ -2,11 +2,29 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 sys.path.append('../')
-from repo_utils import loading_dataset, feature_corr, get_biggest_change_features, repo_path
+from repo_utils import loading_dataset, feature_corr, get_biggest_change_features, repo_path, gh_repos_path
 
 #############################################
 
+def neutralize(predictions: pd.DataFrame, features: pd.DataFrame, proportion: float = 1.0) -> pd.DataFrame:
+    """
+    newer version from v4.2 datasets / from sept. 2023 / Neutralize predictions to features
+    params: df, features, proportion
+    df ...          input df / vector over the features room
+    features ...     array / columns of df
+    proportion ...  scalar 
+    ---------------
+    return: new neutralized df
+    """
+    # add a constant term the features so we can fit the bias/offset term
+    features = np.hstack((features, np.array([np.mean(predictions)] * len(features)).reshape(-1, 1)))
+    print(features)
+    # remove the component of the predictions that are linearly correlated with features
+    return predictions - proportion * features @ (np.linalg.pinv(features, rcond=1e-6) @ predictions)
+
+"""
 df = pd.read_csv(repo_path + "/rounds/" + "val_pred.csv")
 print(len(df))
 x = np.arange(0,len(df))
@@ -19,6 +37,30 @@ y = lambda x : x
 
 plt.plot(x,y(x))
 plt.show()
+"""
+
+feature_metadata = json.load(open(gh_repos_path + "/features.json")) 
+feature_sets = feature_metadata["feature_sets"]
+
+sizes = ["small", "medium", "all"]
+groups = ["intelligence", "wisdom", "charisma", "dexterity", "strength", "constitution", "agility", "serenity", "all"]
+subgroups = {}
+for size in sizes:
+    subgroups[size] = {}
+    for group in groups:
+        # intersection of feature sets
+        subgroups[size][group] = set(feature_sets[size]).intersection(set(feature_sets[group]))
+
+feature_subset = list(subgroups["medium"][group])
+
+filename = "2023-09-28_round2_predictions.csv"
+df = pd.read_csv(repo_path + "/rounds/" + filename)
+#print(df["prediction"])
+
+#print(df.iloc[:,1])
+
+neutralized = df.apply(lambda d: neutralize(d.iloc[:,1], d[feature_subset]))
+
 
 #############################################
 """

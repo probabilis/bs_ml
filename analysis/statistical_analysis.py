bc9775d1f489f122931c108972aa58f437dbff8e
probabilis <m.gschaider@outlook.com>
2023-11-18 18:14:26 +0100

    updated

--- analysis/statistical_analysis.py
+++ analysis/statistical_analysis.py
@@ -8,20 +8,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 import json
+import gc
 #own modules
 sys.path.append('../')
 from repo_utils import gh_repos_path
-from data_loading import loading_datasets
 from statistical_analysis_tools import statistics, plot_statistics, histogram, overall_statistics, plot_correlations
 from preprocessing.cross_validators import era_splitting
 
 #############################################
-
-train, feature_cols, target_cols = loading_datasets()
-
-df = era_splitting(train)
-
 feature_metadata = json.load(open(gh_repos_path + "/features.json")) 
+
+feature_cols = feature_metadata["feature_sets"]["medium"]
+target_cols = feature_metadata["targets"]
+
+#loading training dataset v4.2
+train = pd.read_parquet(gh_repos_path + "/train.parquet", columns=["era"] + feature_cols + target_cols)
+
+#############################################
+#performing subsampling of the initial training dataset due to performance (era splitting)
+train = era_splitting(train)
+#start garbage collection interface / full collection
+gc.collect()
+
+#############################################
 
 feature_sets = feature_metadata["feature_sets"]
 
@@ -39,16 +48,17 @@ t20s = [t for t in target_names if t.endswith("_20")]
 #correlation
 
 print("feature statistics")
-df_st = statistics(df, feature_cols)
+df_st = statistics(train, feature_cols)
 print(df_st)
 
-mean, var = overall_statistics(df, feature_cols)
+mean, var = overall_statistics(train, feature_cols)
 print(mean, var)
 
 #############################################
 
-plot_statistics(df_st,'mean', "train_df_features_mean")
-plot_statistics(df_st,'variance', "train_df_features_variance")
+plot_statistics(df_st,'mean',"feature","train_df_features_mean")
+
+plot_statistics(df_st,'variance',"feature", "train_df_features_variance")
 
 #histogram(df_st['feature_mean'], "train_df_hist_mean")
 #histogram(df_st['feature_variance'], "train_df_hist_var")

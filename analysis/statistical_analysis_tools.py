cc40fcd541666d076d8238ef0712982dbde665fa
probabilis <m.gschaider@outlook.com>
2023-11-18 18:32:21 +0100

    updated

--- analysis/statistical_analysis_tools.py
+++ analysis/statistical_analysis_tools.py
@@ -169,9 +169,9 @@ def plot_correlations(df_correlations, plot_save, name = None) -> None:
     #cmap = sns.diverging_palette(230, 20, as_cmap=True)
     #sns.heatmap(df_correlations, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5}, xticklabels=False, yticklabels=False)
     fig.suptitle(f'{name} correlation matrix over all training eras', fontsize=fontsize_title)
-    sns.heatmap(df_correlations, annot = False, cmap="coolwarm", mask = mask, xticklabels=False, yticklabels=False)
-    plt.xlabel(f'{name} indices')
-    plt.ylabel(f'{name} indices')
+    sns.heatmap(df_correlations, annot = False, cmap="coolwarm", mask = mask, xticklabels=False, yticklabels=False, cbar_kws={'label': 'corr($x_i, x_j$)'})
+    plt.xlabel(f'{name} indices', fontsize = fontsize)
+    plt.ylabel(f'{name} indices', fontsize = fontsize)
     df_correlations.to_csv(repo_path + f"/analysis/{name}_correlations_matrix_{date.today()}.csv")
 
     if plot_save == True:

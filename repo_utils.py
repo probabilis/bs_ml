58763f17dd528d631a294f67940303b1786b7519
probabilis <m.gschaider@outlook.com>
2023-11-19 15:27:17 +0100

    updated

--- repo_utils.py
+++ repo_utils.py
@@ -4,11 +4,16 @@ MN: 12030366
 """
 #official open-source repositories
 import os
+import json
 from pathlib import Path
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import scipy
+import gc
+from numerapi import NumerAPI
+import time
+from preprocessing.cross_validators import era_splitting
 #############################################
 #############################################
 #############################################
@@ -24,7 +29,8 @@ fontsize = 12
 #############################################
 #functions and methods
 
-def loading_dataset():
+def loading():
+
     """
     params: path, filename 
     path ...        str / relative path folders for file 
@@ -36,14 +42,44 @@ def loading_dataset():
     target ....     target vector
     eras   ...      eras vector
     """
-    df = pd.read_parquet(path_)
-    print("Loaded dataframe from path : ", path_)
 
-    features = [f for f in df if f.startswith("feature")]
-    target = "target"
-    df["erano"] = df.era.astype(int)
-    eras = df.erano
-    return df, features, target, eras
+    #numer.AI official API for retrieving and pushing data
+    napi = NumerAPI()
+    #train set
+    napi.download_dataset("v4.2/train_int8.parquet", gh_repos_path + "/train.parquet")
+    #validation set
+    napi.download_dataset("v4.2/validation_int8.parquet", gh_repos_path + "/validation.parquet" )
+    #live dataset 
+    napi.download_dataset("v4.2/live_int8.parquet", gh_repos_path + "/live.parquet")
+    #features metadata
+    napi.download_dataset("v4.2/features.json", gh_repos_path + "/features.json")
+
+    start = time.time()
+
+    feature_metadata = json.load(open(gh_repos_path + "/features.json")) 
+
+    feature_cols = feature_metadata["feature_sets"]["medium"]
+    target_cols = feature_metadata["targets"]
+
+    #loading training dataset v4.2
+    train = pd.read_parquet(gh_repos_path + "/train.parquet", columns=["era"] + feature_cols + target_cols)
+
+    #############################################
+    #performing subsampling of the initial training dataset due to performance (era splitting)
+    train = era_splitting(train)
+    #start garbage collection interface / full collection
+    gc.collect()
+
+    #############################################
+
+    assert train["target"].equals(train["target_cyrus_v4_20"])
+    target_names = target_cols[1:]
+    targets_df = train[["era"] + target_names]
+
+    t20s = [t for t in target_names if t.endswith("_20")]
+    t60s = [t for t in target_names if t.endswith("_60")]
+
+    return train, feature_cols, target_cols, targets_df, t20s, t60s
 
 ##########################
 

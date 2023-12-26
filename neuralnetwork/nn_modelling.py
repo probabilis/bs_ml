522f81c734de9365f1fac29773b862b23da6521b
probabilis <m.gschaider@outlook.com>
2023-12-26 17:35:54 +0100

    vps run

--- neuralnetwork/nn_modelling.py
+++ neuralnetwork/nn_modelling.py
@@ -7,6 +7,7 @@ import gc
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split 
 sys.path.append("../")
 from repo_utils import loading
 
@@ -43,6 +44,9 @@ X_train = torch.tensor(X_train, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+
+del train 
+gc.collect()
 
 print("prepared data sucessfully")
 

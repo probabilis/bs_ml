44eadfd3d729f2c237823b5cc5ac22874fedc17a
probabilis <m.gschaider@outlook.com>
2023-12-29 09:35:28 +0100

    vps run

--- neuralnetwork/gbm_nn_comparison.py
+++ neuralnetwork/gbm_nn_comparison.py
@@ -50,7 +50,7 @@ model_nn.eval()
 st = time.time()
 
 model_gbm = LGBMRegressor(
-        n_estimators = 50,#6352
+        n_estimators = 6352,
         learning_rate = 0.02,
         max_depth = 1,
         colsample_bytree = 0.9
